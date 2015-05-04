cb754ec63eaae2454d6ea90279a0fd849da81ef0
Christopher Haster <chaster@utexas.edu>
2015-05-04 01:47:57 -0500

    Changed tests to latency

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 for ((i = 0 ; i < 2 ; i++)) ; do
 for ((j = 0 ; j < 10 ; j++)) ; do
 for ((k = 0 ; k < 10 ; k++)) ; do
-    sed -i "s/REACTION_TIME:.*\$/REACTION_TIME: ${i}.${j}${k}/" config.conf
-    echo -ne "Reaction Time: ${i}.${j}${k} " >> results.out
+    sed -i "s/LATENCY:.*\$/LATENCY: ${i}.${j}${k}/" config.conf
+    echo -ne "Latency: ${i}.${j}${k} " >> results.out
     python car_net_sim.py 60 | grep "Collisions" >> results.out
 done
 done
